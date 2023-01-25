b9f3fe5a8a29b144e0a847913765fa2d91629352
Aadhavan <aadhavan.pazhani@emsibg.com>
2023-01-25 12:18:00 +0530

    basic calculator class

---
+++ calculator.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+class Calculator:
+
+    def __init__(self) -> None:
+        pass
+
+    def add(self, x: int, y: int) -> int:
+        return x+y
+
+    def multiply(self, x: int, y: int) -> int:
+        return x*y
+
+    def divide(self, x: int, y: int) -> Union(int, float):
+        return x / y
+
+    def subtract(self, x: int, y: int) -> int:
+        return x - y
+
+    
